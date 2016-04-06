bc3050e5c0fd93c3eb7c9913ea443439e560e4d2
Jiao Lin <linjiao@ornl.gov>
2016-04-06 12:24:07 -0400

    Refs #2. forward_TestCase2.py and forward_TestCase3.py passed

--- tests/forward_TestCase3.py
+++ tests/forward_TestCase3.py
@@ -4,13 +4,7 @@
 
 skip = True
 
-import unittestX as unittest
-import journal
-
-#debug = journal.debug( "TestCase" )
-#warning = journal.warning( "TestCase" )
-
-
+import unittest
 import numpy
 
 
@@ -18,17 +12,15 @@ class TestCase(unittest.TestCase):
 
 
     def test1(self):
-        "multiphonon.computeAnESet: UN N dos"
-        dos = readdos()
-        E = dos.energy
+        "multiphonon.forward.computeAnESet: UN N dos"
+        E,g = readdos()
         dE = E[1] - E[0]
-        g = dos.I
         # expand E a bit
         E = numpy.arange(E[0], 500, dE)
         g = numpy.concatenate((g, numpy.zeros(len(E)-len(g))))
         
         g/=g.sum()*dE
-        from mccomponents.sample.phonon.multiphonon import computeAnESet
+        from multiphonon.forward import computeAnESet
         kelvin2mev = 0.0862
         beta = 1./(5*kelvin2mev)
         E, An_set = computeAnESet(N=10, E=E, g=g, beta=beta, dE=dE)
@@ -41,13 +33,11 @@ class TestCase(unittest.TestCase):
         
         
     def test2(self):
-        "multiphonon.sqe: UN N dos"
-        dos = readdos()
-        E = dos.energy
+        "multiphonon.forward.sqe: UN N dos"
+        E, g= readdos()
         dE = E[1] - E[0]
-        g = dos.I
         #
-        from mccomponents.sample.phonon.multiphonon import sqe
+        from multiphonon.forward import sqe
         q,e,i = sqe(E, g, T=5, M=14, N=7, Qmax=45.)
         from histogram import plot, histogram
         axes = [('Q', q, 'angstrom**-1'), ('E', e, 'meV')]
@@ -55,15 +45,14 @@ class TestCase(unittest.TestCase):
         plot(iqe)
         return
         
-        
     pass  # end of TestCase
 
 
 def readdos():
-    from mccomponents.sample.phonon.read_dos import doshist_fromascii
     datapath = 'UN-N-dos.dat'
-    dos = doshist_fromascii(datapath)
-    return dos
+    from multiphonon.dos import io
+    E, Z, error = io.fromascii(datapath)
+    return E,Z
 
 
 if __name__ == "__main__": unittest.main()
