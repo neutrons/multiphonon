6c0a7e75e0a65500da95d87d52a9f169de2fc997
Jiao Lin <linjiao@ornl.gov>
2016-04-29 15:26:51 -0400

    Refs #5. added function .ms.sqe

---
+++ multiphonon/ms.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#
+# Jiao Lin
+
+
+"""
+compute multiple scattering
+"""
+
+
+def sqe(mpsqe, Ei):
+    """given multiphonon SQE, compute multiple scattering sqe
+    """
+    # multiple scattering (MS) is uniform along Q
+    # so we want to compute the average S from multi-phonon
+    # scattering and assign the value to MS result
+    # first compute the mask
+    from .sqe import dynamical_range_mask
+    import numpy as np
+    mask = dynamical_range_mask(mpsqe, Ei)
+    # set outside to zero
+    mpsqe.I[mask] = 0
+    # average
+    aveS = mpsqe.I.sum(0)/np.logical_not(mask).sum(0)
+    # res
+    mssqe = mpsqe.copy()
+    mssqe.I[:] = aveS[np.newaxis, :]
+    mssqe.I[mask] = np.nan
+    return mssqe
+
+
+# End of file 
---
+++ tests/ms_TestCase.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#
+
+
+interactive = False
+
+import os
+datadir = os.path.join(os.path.dirname(__file__), "data")
+
+import unittest
+import numpy as np, histogram.hdf as hh, histogram as H
+from multiphonon import ms
+
+
+class TestCase(unittest.TestCase):
+
+
+    def test1(self):
+        "multiphonon.ms"
+        mpsqe = hh.load(os.path.join(datadir, "V-S2..5.h5"))
+        mssqe = ms.sqe(mpsqe, Ei=110.)
+        if interactive:
+            H.plot(mssqe, min=0)
+        return
+        
+        
+    pass  # end of TestCase
+
+
+if __name__ == "__main__":
+    interactive = True
+    unittest.main()
+    
+# End of file 
