a17712467b6b56cb8f883598dc568f472d7896a0
Jiao Lin <jiao.lin@gmail.com>
2016-05-06 06:19:29 -0400

    Refs #12. added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+
+from setuptools import setup, find_packages
+
+# define distribution
+setup(
+    name = "multiphonon",
+    version = "0.1",
+    packages = find_packages(".", exclude=['tests', 'docs']),
+    package_dir = {'': "."},
+    test_suite = 'tests',
+    install_requires = [
+        'numpy',
+    ],
+    dependency_links = [
+    ],
+    author = "SNS-chops team",
+    description = "Multiphonon scattering and multiple scattering correction for powder data",
+    license = 'BSD',
+    keywords = "neutron, inelastic neutron scattering, powder, phonon",
+    url = "https://github.com/sns-chops/multiphonon",
+    # download_url = '',
+)
+
