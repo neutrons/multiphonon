b887bb3043ebe76dd13de41f8b935c38044f5135
Jiao Lin <linjiao@ornl.gov>
2016-11-29 12:31:57 -0500

    multiphonon/getdos.py: improved interface

--- multiphonon/getdos.py
+++ multiphonon/getdos.py
@@ -1,12 +1,23 @@
 import histogram.hdf as hh, numpy as np
 import os
 
+# hide the warnings on divide by zero etc
+np.seterr(divide='ignore', invalid='ignore')
+
+
 def getDOS(sample_nxs, mt_nxs=None, mt_fraction=0.9,
            Emin=-100, Emax=100, dE=1.,
            Qmin=0, Qmax=15., dQ=0.1, T=300, Ecutoff=50., 
            elastic_E_cutoff=(-20., 7), M=50.94,
            C_ms=0.3, Ei=116.446, workdir='work',
-           iqe_nxs="iqe.nxs", iqe_h5="iqe.h5"):
+           iqe_nxs="iqe.nxs", iqe_h5="iqe.h5", maxiter=10):
+    """compute DOS from powder spectrum by performing multiphonon and 
+    multiple-scattering corrections.
+    This is an iterator. so call it with an evaluation of the iteration.
+    For example:
+
+      >>> output = list(getDOS(...))
+    """
     # prepare paths
     if not os.path.exists(workdir):
         os.makedirs(workdir)
@@ -17,6 +28,7 @@ def getDOS(sample_nxs, mt_nxs=None, mt_fraction=0.9,
     # reduce
     Eaxis = Emin, Emax, dE
     Qaxis = Qmin, Qmax, dQ
+    yield "Convert sample data to powder I(Q,E)"
     raw2iqe(sample_nxs, iqe_nxs, iqe_h5, Eaxis, Qaxis)
     iqehist = hh.load(iqe_h5)
     if mt_nxs is not None:
@@ -24,6 +36,7 @@ def getDOS(sample_nxs, mt_nxs=None, mt_fraction=0.9,
             os.path.dirname(p), 'mt-'+os.path.basename(p))
         mtiqe_nxs = _tomtpath(iqe_nxs)
         mtiqe_h5 = _tomtpath(iqe_h5)
+        yield "Convert MT data to powder I(Q,E)"
         raw2iqe(mt_nxs, mtiqe_nxs, mtiqe_h5, Eaxis, Qaxis)
         iqehist -= hh.load(mtiqe_h5) * (mt_fraction, 0)
     # to DOS
@@ -35,21 +48,24 @@ def getDOS(sample_nxs, mt_nxs=None, mt_fraction=0.9,
     hh.dump(newiqe, 'iqe-interped.h5')
     # create processing engine
     from .backward import sqe2dos
-    print("iterative computation of DOS...")
     iterdos = sqe2dos.sqe2dos(
-      newiqe, T=T, Ecutoff=Ecutoff, 
-      elastic_E_cutoff=elastic_E_cutoff, M=M,
-      C_ms=C_ms, Ei=Ei, workdir='work')
-    doslist = list(iterdos)
-    print("done.")
-    return doslist
+        newiqe, T=T, Ecutoff=Ecutoff, 
+        elastic_E_cutoff=elastic_E_cutoff, M=M,
+        C_ms=C_ms, Ei=Ei, workdir='work',
+        MAX_ITERATION=maxiter)
+    doslist = []
+    yield "Iterative computation of DOS..."
+    for i,dos in enumerate(iterdos):
+        yield "Finished round #%s" % (i+1,)
+        continue
+    yield "Done"
+    return
 
 
 def raw2iqe(eventnxs, iqe_nxs, iqe_h5, Eaxis, Qaxis):
     Emin, Emax, dE = Eaxis
     Qmin, Qmax, dQ = Qaxis
     # reduce
-    print("reducing...")
     if not os.path.exists(iqe_nxs):
         cmd = "mcvine instruments arcs nxs reduce "
         cmd += "%(eventnxs)s --out=%(iqe_nxs)s "
@@ -60,7 +76,6 @@ def raw2iqe(eventnxs, iqe_nxs, iqe_h5, Eaxis, Qaxis):
         if os.system(cmd):
             raise RuntimeError("%s failed" % cmd)
     # to histogram
-    print("to histogram...")
     if not os.path.exists(iqe_h5):
         cmd = "mcvine mantid extract_iqe %(iqe_nxs)s %(iqe_h5)s" % locals()
         if os.system(cmd):
@@ -107,7 +122,13 @@ def notebookUI(samplenxs, mtnxs, options=None, load_options_path=None):
     )
 
     w_Run = widgets.Button(description="Run")
+    w_all = w_inputs + (w_Run,)
+
     def submit(b):
+        # suppress warning from h5py
+        import warnings
+        warnings.simplefilter(action = "ignore", category = FutureWarning)
+        #
         kargs = dict(
             mt_fraction = w_mt_fraction.value,
             Emin=w_Emin.value, Emax=w_Emax.value, dE=w_dE.value,
@@ -119,9 +140,9 @@ def notebookUI(samplenxs, mtnxs, options=None, load_options_path=None):
             workdir=w_workdir.value,
             )
         import pprint, os, yaml
-        pprint.pprint(samplenxs)
-        pprint.pprint(mtnxs)
-        pprint.pprint(kargs)
+        # pprint.pprint(samplenxs)
+        # pprint.pprint(mtnxs)
+        # pprint.pprint(kargs)
         workdir = kargs['workdir']
         if not os.path.exists(workdir):
             os.makedirs(workdir)
@@ -130,12 +151,66 @@ def notebookUI(samplenxs, mtnxs, options=None, load_options_path=None):
         options['ElasticPeakMax']=w_ElasticPeakMax.value
         yaml.dump(options, 
                   open(os.path.join(workdir, 'getdos-opts.yaml'), 'wt'))
-        getDOS(samplenxs, mt_nxs=mtnxs, **kargs)
+        maxiter = 10
+        close = lambda w: w.close()
+        map(close, w_all)
+        log_progress(getDOS(samplenxs, mt_nxs=mtnxs, maxiter=maxiter, **kargs), every=1, size=maxiter+2)
         return
     w_Run.on_click( submit )
-    w_all = w_inputs + (w_Run,)
     display(*w_all)
     return
+
+
+# modified from https://github.com/alexanderkuk/log-progress
+def log_progress(sequence, every=None, size=None):
+    from ipywidgets import IntProgress, HTML, VBox
+    from IPython.display import display
+
+    is_iterator = False
+    if size is None:
+        try:
+            size = len(sequence)
+        except TypeError:
+            is_iterator = True
+    if size is not None:
+        if every is None:
+            if size <= 200:
+                every = 1
+            else:
+                every = int(size / 200)     # every 0.5%
+    else:
+        assert every is not None, 'sequence is iterator, set every'
+
+    if is_iterator:
+        progress = IntProgress(min=0, max=1, value=1)
+        progress.bar_style = 'info'
+    else:
+        progress = IntProgress(min=0, max=size, value=0)
+    label = HTML()
+    box = VBox(children=[label, progress])
+    display(box)
+
+    index = 0
+    try:
+        for index, msg in enumerate(sequence, 1):
+            if index == 1 or index % every == 0:
+                if is_iterator:
+                    label.value = 'Running: {index} / ?: {msg}...'.format(index=index, msg=msg)
+                else:
+                    progress.value = index
+                    label.value = u'Running: {index} / {size}: {msg}...'.format(
+                        index=index,
+                        size=size,
+                        msg=msg
+                    )
+    except:
+        progress.bar_style = 'danger'
+        raise
+    else:
+        progress.bar_style = 'success'
+        progress.value = size
+        # label.value = str(index or '?')
+        label.value = 'Done.'
 
 
 default_options = dict(
