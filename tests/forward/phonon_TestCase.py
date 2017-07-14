neutrons/multiphonon

commit 7b4ddfb9f7ac8da4a1262910fa7581216d69d41a

Refs #29. eliminate NaNs in coth

multiphonon/forward/phonon.py
@@ -224,7 +224,11 @@ def reflected(x,y):
 
     
 def coth(x):
-    return np.cosh(x)/np.sinh(x)
+    tentative = np.cosh(x)/np.sinh(x)
+    tentative[x>10] = 1.
+    tentative[x<10] = -1
+    return tentative
+
 
 def gamma0(E, g, beta, dE):
     """Compute gamma0
@@ -237,7 +241,10 @@ def gamma0(E, g, beta, dE):
     """
     assert abs(E[0]) < 1e-7 # E[0] must be 0
     dos_integrated = np.sum(g)*dE
-    assert abs(dos_integrated - 1) < 1e-3
+    if not abs(dos_integrated - 1) < 1e-3:
+        # import pickle as pkl
+        # pkl.dump((E,g,beta,dE), open('mp.forward.phonon.gamma0-debug.pkl', 'w'))
+        raise RuntimeError("integrated dos should be 1, got %s instead" % (dos_integrated,))
     # compute function to integrate
     f = coth(beta * E/2.) * g/E
     # f[0] would be nan, replace that with "extrapolation"

commit 478b60c9528ef0f88db5b124d85bc5eedbff7d77

fixed a bug introduced in 7b4ddfb. added check in a test

multiphonon/forward/phonon.py
@@ -226,7 +226,7 @@ def reflected(x,y):
 def coth(x):
     tentative = np.cosh(x)/np.sinh(x)
     tentative[x>10] = 1.
-    tentative[x<10] = -1
+    tentative[x<-10] = -1
     return tentative
 
 

tests/forward/phonon_TestCase.py
@@ -8,7 +8,7 @@ import sys, os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "data"))
 
 import unittest
-import numpy
+import numpy as np
 
 
 class TestCase(unittest.TestCase):
@@ -20,13 +20,15 @@ class TestCase(unittest.TestCase):
         E, g = loadDOS()
         dE = E[1] - E[0]
         # expand E a bit
-        E = numpy.arange(E[0], 70, dE)
-        g = numpy.concatenate((g, numpy.zeros(len(E)-len(g))))
+        E = np.arange(E[0], 70, dE)
+        g = np.concatenate((g, np.zeros(len(E)-len(g))))
         g/=g.sum()*dE
         from multiphonon.forward.phonon import computeAnESet
         kelvin2mev = 0.0862
         beta = 1./(300*kelvin2mev)
         E, An_set = computeAnESet(N=5, E=E, g=g, beta=beta, dE=dE)
+        self._check(E, np.load('expected_results/phonon.test1.E.npy'))
+        self._check(An_set, np.load('expected_results/phonon.test1.An_set.npy'))
         if interactive:
             import pylab
             for An in An_set:
@@ -42,12 +44,12 @@ class TestCase(unittest.TestCase):
         E,g = loadDOS()
         # expand E a bit
         dE = E[1] - E[0]
-        E = numpy.arange(E[0], 70, dE)
-        g = numpy.concatenate((g, numpy.zeros(len(E)-len(g))))
-        int_g = numpy.sum(g) * dE
+        E = np.arange(E[0], 70, dE)
+        g = np.concatenate((g, np.zeros(len(E)-len(g))))
+        int_g = np.sum(g) * dE
         g/=int_g
         
-        Q = numpy.arange(0, 10, 0.1)
+        Q = np.arange(0, 10, 0.1)
         dQ = Q[1] - Q[0]
         
         kelvin2mev = 0.0862
@@ -79,6 +81,10 @@ class TestCase(unittest.TestCase):
         Q, E, S = sqe(E,g, N=4)
         saveSQE(Q,E,S, 'S_2..5')
         return
+
+
+    def _check(self, a1, a2):
+        self.assert_(np.allclose(a1, a2))
         
         
     pass  # end of TestCase