934a0e34ac05ec6a1ab1cfb14418c8686f62c6dc
Jiao Lin <jiao.lin@gmail.com>
2021-08-15 20:04:59 -0700

    added method "dos.interp" and a test

---
+++ tests/dos/interp_TestCase.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#
+
+interactive = False
+
+import os, numpy as np, histogram.hdf as hh
+here = os.path.dirname(__file__)
+
+from multiphonon.dos import interp
+
+import unittest
+class TestCase(unittest.TestCase):
+
+    def test1(self):
+        "interp"
+        dos = hh.load(os.path.join(here, 'dos_to_interp.h5'))
+        newE = np.arange(0, 45, 0.5)
+        newdos = interp(dos, newE)
+        expected = hh.load(os.path.join(here, './expected/dos_interped.h5'))
+        np.testing.assert_allclose(newdos.I, expected.I)
+        np.testing.assert_allclose(newdos.E2, expected.E2)
+        return
+    pass  # end of TestCase
+
+
+if __name__ == "__main__":
+    interactive = True
+    unittest.main()
+
+# End of file
